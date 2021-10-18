5624a4e5bd66afcb0ee8cf1d376f32b316aed0ff
amiya0496 <amiya@digitalaptech.co.in>
2021-10-18 13:50:29 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+export const BASE_URL = 'http://34.236.201.29:4000';
+//export const BASE_URL = 'http://localhost:4000';
+// export const BASE_URL = 'https://app.admisure.com';
+// export const BASE_IMAGE_UPLOAD =
+//   'https://cors-anywhere.herokuapp.com/https://nodejsdapldevelopments.com/admisure/public';
+export const BASE_IMAGE_UPLOAD = 'https://nodejsdapldevelopments.com/admisure/public';
+  //export const BASE_IMAGE_UPLOAD = 'https://coaching.admisure.com';
